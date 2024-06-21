9ade3f442b837ef4a11b07fb6ef06e3b4a958f0b
NiravNanu <160338445+NiravNanu@users.noreply.github.com>
2024-06-21 18:28:59 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+    region = "us-east-1"
+  
+}
+
+resource "aws_instance" "myec2" {
+    ami = "ami-04b70fa74e45c3917"
+    instance_type = "t2.medium"
+    user_data = file(script.sh)
+}
