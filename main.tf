a4e13ff9d910e7da049261098bc44514a3b59f84
NiravNanu <160338445+NiravNanu@users.noreply.github.com>
2024-06-21 20:48:35 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,5 +6,5 @@ provider "aws" {
 resource "aws_instance" "myec2" {
     ami = "ami-04b70fa74e45c3917"
     instance_type = "t2.medium"
-    echo "Hi Nirav Shah" >/var/www/html/index.nginx-debian.html
+    user_data     = file("script.sh")
 }
